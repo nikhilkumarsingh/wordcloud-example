47af687257971b41d5ec030c8e1c8c49ee0bbad7
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-09-10 12:01:00 +0530

    Initial commit

---
+++ mywc.py
@@ -0,0 +1,44 @@
+from os import path
+import numpy as np
+from PIL import Image
+import wikipedia
+from wordcloud import WordCloud, STOPWORDS
+
+# get path to script's directory
+currdir = path.dirname(__file__)
+
+def get_wiki(query):
+	# get best matching title for given query
+	title = wikipedia.search(query)[0]
+
+	# get wikipedia page for selected title
+	page = wikipedia.page(title)
+	return page.content
+
+
+def create_wordcloud(text):
+	# create numpy araay for wordcloud mask image
+	mask = np.array(Image.open(path.join(currdir, "cloud.png")))
+
+	# create set of stopwords	
+	stopwords = set(STOPWORDS)
+
+	# create wordcloud object
+	wc = WordCloud(background_color="white",
+					max_words=200, 
+					mask=mask,
+	               	stopwords=stopwords)
+	
+	# generate wordcloud
+	wc.generate(text)
+
+	# save wordcloud
+	wc.to_file(path.join(currdir, "wc.png"))
+
+
+if __name__ == "__main__":
+	# get text for article
+	text = get_wiki("python programming language")
+	
+	# generate wordcloud
+	create_wordcloud(text)
